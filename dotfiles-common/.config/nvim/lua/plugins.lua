d8ee921fb9d7586b79248f6f5ca66fc19efb0f5d
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2026-02-04 15:19:25 +0000

    Make Neovim system clipboard work over SSH

--- dotfiles-common/.config/nvim/lua/plugins.lua
+++ dotfiles-common/.config/nvim/lua/plugins.lua
@@ -410,3 +410,16 @@ vim.api.nvim_create_autocmd({
 	end,
 	desc = "Check for file changes on disk",
 })
+
+-- Make system clipboard work over SSH
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
