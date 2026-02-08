270ddf1292fa76fa443801fbc92e9f2714e2bf02
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2026-02-08 19:50:39 +0100

    Update Neovim

--- dotfiles-common/.config/nvim/lua/plugins.lua
+++ dotfiles-common/.config/nvim/lua/plugins.lua
@@ -413,13 +413,13 @@ vim.api.nvim_create_autocmd({
 
 -- Make system clipboard work over SSH
 vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  },
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
 }
