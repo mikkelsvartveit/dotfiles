400061d5825bb4a831a814ae0a46d202282263f2
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2026-04-06 15:36:10 +0200

    Update Neovim

--- dotfiles-common/.config/nvim/lua/plugins.lua
+++ dotfiles-common/.config/nvim/lua/plugins.lua
@@ -145,7 +145,7 @@ require("lazy").setup({
 					null_ls.builtins.formatting.gofmt,
 				},
 				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
+					if client:supports_method("textDocument/formatting") then
 						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 						vim.api.nvim_create_autocmd("BufWritePre", {
 							group = augroup,
